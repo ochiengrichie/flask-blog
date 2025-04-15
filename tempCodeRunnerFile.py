2344e7c5dbd5728b714e1071a0602a42a947d740
Richard Ochieng Onyango <ochiengrichie4@gmail.com>
2025-04-15 13:55:11 +0300

    Added gunicorn and Procfile for production

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+class Comment(db.model):
+#     __tablename__ = "comments"
+#     id:Mapped[int] = mapped_column(Integer, primary_key=True)
+#     text:Mapped[str] = mapped_column(Text,nullable=False )
